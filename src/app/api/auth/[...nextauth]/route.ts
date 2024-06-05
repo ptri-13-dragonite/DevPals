4821a3bce53e153b2d0cfad95b74b066f51bb2c7
Mike Basta <mikebasta@gmail.com>
2024-06-05 09:51:17 -0700

    next-auth framework added with GitHub provider config

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.JWT_SECRET as string,
+});
+
+export { handler as GET, handler as POST };
