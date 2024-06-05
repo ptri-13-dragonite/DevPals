8d0bfbf89a5b6e97840c8d9f58cbbc99bcdaafdb
James Shea <jshea915@gmail.com>
2024-06-05 11:59:04 -0600

    landing page completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ function Navbar() {
       <div className="flex-1">
         <a href="/" className="btn btn-ghost text-xl">daisyUI</a>
       </div>
-      <div className="flex-none gap-2">
+      <div className="flex-3 gap-2">
         <div className="dropdown dropdown-end">
           <button type="button" className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
@@ -32,19 +32,6 @@ function Navbar() {
             <li><a href="/logout">Logout</a></li>
           </ul>
         </div>
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <details>
-              <summary>
-                Parent
-              </summary>
-              <ul className="p-2 bg-base-100 rounded-t-none">
-                <li><a href="/link1">Link 1</a></li>
-                <li><a href="/link2">Link 2</a></li>
-              </ul>
-            </details>
-          </li>
-        </ul>
       </div>
     </div>
   );
