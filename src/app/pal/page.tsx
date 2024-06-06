5f1b82ecf93163350795fd5695de0d57b67422f4
GregSilvia <iamgregsilvia@gmail.com>
2024-06-06 15:46:37 -0700

    render environment

--- src/app/pal/page.tsx
+++ src/app/pal/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+
+'use client';
+
+import React, { useEffect } from 'react';
+import { Application } from '@splinetool/runtime';
 
 function PalEnv() {
+  useEffect(() => {
+    // HTMLCanvasElement
+    const canvas = document.getElementById('canvas3d') as HTMLCanvasElement;
+    const app = new Application(canvas);
+    app.load('https://prod.spline.design/OBzWqD9Jh-uSqH9Z/scene.splinecode')
+      .then(() => {
+        console.log('Spline scene loaded');
+      })
+      .catch((error) => {
+        console.error('Error loading Spline scene:', error);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>PalEnv</h1>
+    <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
+      <h1 style={{ position: 'absolute', top: '20px', left: '20px', color: 'white' }}>PalEnv</h1>
+      <canvas id="canvas3d" style={{ width: '100%', height: '100%' }} />
     </div>
   );
 }
